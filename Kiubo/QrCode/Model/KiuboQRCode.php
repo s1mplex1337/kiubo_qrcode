3142250e328d1e4e38af70f1a2c13bd96bb663ec
s1mplex1337 <73103042+s1mplex1337@users.noreply.github.com>
2023-08-10 13:14:58 +0200

    endroid fix 4+

--- Kiubo/QrCode/Model/KiuboQRCode.php
+++ Kiubo/QrCode/Model/KiuboQRCode.php
@@ -3,12 +3,17 @@ namespace Kiubo\QrCode\Model;
 
 use Kiubo\QrCode\Api\QRCodeInterface;
 use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Response\QrCodeResponse;
 
 class KiuboQRCode implements QRCodeInterface
 {
     public function generateQRCode($url)
     {
-        $qrCode = new QrCode($url);
-        return $qrCode->writeDataUri();
+        //$qrCode = new QrCode($url);
+        //return $qrCode->writeDataUri();
+        $qrCode = QrCode::create($url);
+        $dataUri = $qrCode->writeDataUri();
+
+        return $dataUri;
     }
 }
